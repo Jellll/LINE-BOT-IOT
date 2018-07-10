0ac2268e1a70db7a97b152037d79281645336fe8
Jellll <40591937+Jellll@users.noreply.github.com>
2018-07-10 15:23:07 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,13 +9,11 @@ $access_token = '3yPQxnFy4p11esHXRUKcPAkGeBt0uwSbgSk/nSK0JBhUQM5icUI19j4LGT4LHjU
 $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['ESP'])) {
-	//if (!is_null($events['ID'])) {
-	//	$id = $events['ID']
-	//	send_LINE($id.':'.$events['ESP']);// เรียกฟังชั่นที่ Line.php
-	//}else
+	if (isset($_GET["ID"])) {
+		$id = $_GET["ID"];
+		send_LINE($id.':'.$events['ESP']);// เรียกฟังชั่นที่ Line.php
+	}else
 	{
-
-	
 	send_LINE($events['ESP']);// เรียกฟังชั่นที่ Line.php
 		
 	echo "OK";
